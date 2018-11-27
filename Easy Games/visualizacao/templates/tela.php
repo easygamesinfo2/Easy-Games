5dca3e61f6f6256a2a688802312eb5f22c96358d
Guaxinim456 <galeguinho3365@gmail.com>
2018-11-27 15:23:29 -0200

    Add files via upload

---
+++ Easy Games/visualizacao/templates/tela.php
@@ -0,0 +1,45 @@
+<html><head>
+  <!-- Standard Meta -->
+    <!--zxfghjk-->
+    
+  <meta charset="UTF-8">
+
+  <link rel="stylesheet" type="text/css" href="templates/semantic/semantic.css">
+
+  <style type="text/css">
+
+    .ui.masthead.segment{
+      min-height: 595px;
+      padding: 1em 0em;
+    }
+    
+
+  </style>
+
+
+</head>
+
+<body class="pushable">
+
+<!-- Page Contents -->
+  <div class="ui inverted vertical masthead center aligned segment"
+  style="background-image: url(https://image.freepik.com/vetores-gratis/fundo-sem-costura-de-videogame_6997-111.jpg); ">
+
+    <!--<div class="ui container">
+      <div class="ui large secondary inverted pointing menu">
+        <div class="right item">
+          <a class=" ui  inverted teal button">Log in</a>
+          <a class=" ui inverted teal button">Sign Up</a>
+        </div>
+      </div>
+    </div>-->
+    <br>
+    <br>
+    <br>
+
+    <div class="ui text container" style="margin-top: 100px;">
+      	<img class="ui large image" style="margin-left: auto; margin-right: auto;  " src="../visualizacao/imagenseg/Easy Gaming.png">
+      	<h2 class="ui header" style="color: black">Trazendo as novidades dos Games</h2>
+    </div>
+
+  </div>
